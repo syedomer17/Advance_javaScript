b4435cb6a4941a533007900e6cc30f1e6f5ac3ae
syedomer17 <syedomeali2006@gmail.com>
2024-11-19 07:43:56 +0530

    Advance

--- for-of-loops.js
+++ for-of-loops.js
@@ -7,4 +7,9 @@ for(let i of number){//const or let here (line 13) is MUST ;
 // because it will take var by default
     console.log(i);
 }
-// for of loop is work only in string and array
+// for of loop is work only in string and arralet 
+
+let str = "hello";
+for(let i of str){
+    console.log(i);
+}
